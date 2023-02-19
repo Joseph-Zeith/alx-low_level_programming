1962963783602342b0c826a6e07dc6e0268d9133
Joseph-Zeith <josephzeith@gmail.com>
2023-02-19 13:08:02 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	for (b = 0; b <= 10; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
