74ab9b8c365a4c5535fe77e2573a63ab9cc8c5ac
Joseph-Zeith <josephzeith@gmail.com>
2023-02-19 16:00:01 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ */
+
+int main(void)
+{
+	char k;
+
+	int l;
+
+	k = 'a';
+	l = '0';
+	while
+		(l < 10) {
+			putchar(l + '0');
+			l++;
+		}
+	while
+		(k <= 'f') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
