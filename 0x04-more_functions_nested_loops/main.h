9292e474ba21891c032743ae45a99de37dd373aa
Joseph-Zeith <josephzeith@gmail.com>
2023-02-23 10:18:09 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Ainebyoona Joseph
+ * Desc: Header contains phototypes for all functions
+ *       used in 0x04-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
