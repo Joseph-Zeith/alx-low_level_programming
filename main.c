2a10b94cfbf18cfaa3a9e8ac46ba3b773ef5b0cd
Joseph-Zeith <josephzeith@gmail.com>
2023-03-14 06:45:12 -0700

    operations

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(){
+	int a = 20;
+	int b = 10;
+	printf("%d + %d = %d\n", a, b, (a + b));
+	printf("%d - %d = %d\n", a, b, (a - b));
+	printf("%d / %d = %d\n", a, b, (a / b));
+	printf("%d * %d = %d\n", a, b, (a * b));
+       return 0;
+}       
