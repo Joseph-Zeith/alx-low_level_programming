2c188e60ad75a0bbbc09d3c06f0c1a28c109d7be
Joseph-Zeith <josephzeith@gmail.com>
2023-03-24 20:26:38 -0700

    going deeper into the logic

---
+++ JZ_practice/search.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[10], val, count;
+
+	for (count = 0; count < 10; ++count)
+	{
+		printf("a[%d] = ", count);
+		scanf("%d",&a[count]);
+	}
+	printf("Enter the number to search ");
+	scanf("%d", &val);
+	for (count = 0; count < 10; ++count)
+	{
+		if (val == a[count])
+		{
+			printf("Found at the location %d\n", count + 1);
+			break;
+		}
+	}
+			if (count == 10)
+			printf("The entered value is invalid, please try again");
+}
